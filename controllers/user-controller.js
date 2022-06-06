34e27a2dff4056cc775c93fbeb4c5788f51a4941
kevin-foreman <kevin.o.foreman2@gmail.com>
2022-06-06 13:13:46 -0500

    Finalized controllers and routes, runs successfully localhost

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -36,7 +36,41 @@ const userController = {
             console.log(err);
             res.status(400).json(err);
         });
+    },
+
+    // Create a user 
+    createUser({ body }, res) {
+        User.create(body)
+        .then(dbUserData => res.json(dbUserData))
+        .catch(err => res.status(400).json(err));
+    },
+
+    // Update a user's information
+    updateUser({ params, body }, res) {
+        User.findOneAndUpdate({ _id: params.id }, body, { new: true, runValidators: true })
+        .then(dbUserData => {
+            if (!dbUserData) {
+                res.status(404).json({ message: 'No user found with that ID.'});
+                return;
+            }
+            res.json(dbUserData);
+        })
+        .catch(err => res.status(400).json(err));
+    },
+
+    // Delete a user
+    deleteUser({ params }, res) {
+        User.findOneAndDelete({ _id: params.id })
+        .then(dbUserData => {
+            if (!dbUserData) {
+                res.status(404).json({ message: 'No user found with that ID.' });
+                return;
+            }
+            res.json(dbUserData);
+        })
+        .catch(err => res.status(400).json(err));
     }
+
 }
 
 module.exports = userController;
