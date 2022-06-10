3b99ef94bb41f73f2397356a9bb1b410ec2a4880
kevin-foreman <kevin.o.foreman2@gmail.com>
2022-06-09 20:18:11 -0500

    final push after video creation

--- controllers/thought-controller.js
+++ controllers/thought-controller.js
@@ -79,7 +79,7 @@ const thoughtController = {
     removeReaction({ params }, res) {
         Thought.findOneAndUpdate(
             { _id: params.thoughtId },
-            { $pull: { reactions: { reactionId: body.reactionId } } },
+            { $pull: { reactions: { reactionId: params.reactionId } } },
             { new: true, runValidators: true }
         )
         .then(dbThoughtData => {
